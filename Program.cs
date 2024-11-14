fd6a6dd89978a81ed4d93aaf460f8a0b13eb392e
eldobay <92310342+eldobay@users.noreply.github.com>
2024-11-14 19:55:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+
+namespace assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int smallCarpet = 25 ;
+            int largeCarpet = 35 ;
+            double tax = .06 ;
+            Console.WriteLine("number of small carpet : ");
+            int numberofsmallcarpet = int.Parse(Console.ReadLine());
+            Console.WriteLine($"number of small carpet : {numberofsmallcarpet} ");
+            int costofsmallcarpet = smallCarpet * numberofsmallcarpet;
+             Console.WriteLine(costofsmallcarpet);
+
+
+            Console.WriteLine("number of large carpet :  ");
+            int numberoflargecarpet = int.Parse(Console.ReadLine());
+            Console.WriteLine($"number of large carpet : {numberoflargecarpet} ");
+            int costoflargecarpet = largeCarpet * numberoflargecarpet;
+            Console.WriteLine(costoflargecarpet);
+
+            double thetotalprice = costofsmallcarpet + costoflargecarpet + tax ;
+            Console.WriteLine($"the total price is  : {thetotalprice}");
+
+
+
+        }
+    }
+}
